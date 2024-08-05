fb8c68cdb6e9fc9779a3778cb9ba32cd573950b1
SANJAYYRAJ <727722euec161@gmail.com>
2024-08-05 21:00:21 +0530

    Mod

--- breakout.js
+++ breakout.js
@@ -1,14 +1,14 @@
-
+//To initialise the bacground Board
 let board;
-let boardWidth = 500;
-let boardHeight = 500;
+let boardWidth = 750;
+let boardHeight = 600;
 let context; 
 
-
+//paddle
 let playerWidth = 80;
 let playerHeight = 10;
-let playerVelocityX = 30;
-
+let playerVelocityX = 40;
+//paddle position and behaviour
 let player = {
     x : boardWidth/2 - playerWidth/2,
     y : boardHeight - playerHeight - 5,
@@ -17,12 +17,12 @@ let player = {
     velocityX : playerVelocityX
 }
 
-
+//bad 
 let ballWidth = 10;
 let ballHeight = 10;
 let ballVelocityX = 3;
 let ballVelocityY = 2; 
-
+//ball posittion and behaviour
 let ball = {
     x : boardWidth/2,
     y : boardHeight/2,
@@ -32,22 +32,22 @@ let ball = {
     velocityY : ballVelocityY
 }
 
-
+//dashing blocks
 let blockArray = [];
 let blockWidth = 50;
 let blockHeight = 10;
-let blockColumns = 8; 
-let blockRows = 3; 
+let blockColumns = 12; 
+let blockRows = 5; 
 let blockMaxRows = 10; 
 let blockCount = 0;
 
-
+//blockdirection
 let blockX = 15;
 let blockY = 45;
-
+//scoreboard
 let score = 0;
 let gameOver = false;
-
+//function to be executed on directing to the page
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -55,10 +55,10 @@ window.onload = function() {
     context = board.getContext("2d"); 
 
     
-    context.fillStyle="skyblue";
+    context.fillStyle="green";//for paddle
     context.fillRect(player.x, player.y, player.width, player.height);
 
-    requestAnimationFrame(update);
+    requestAnimationFrame(update);//this updates for every restart of the game
     document.addEventListener("keydown", movePlayer);
 
  
@@ -71,13 +71,13 @@ function update() {
     if (gameOver) {
         return;
     }
-    context.clearRect(0, 0, board.width, board.height);
+    context.clearRect(0, 0, board.width, board.height);//clears for every moving of paddle
 
 
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "blue";//paddle drawing
     context.fillRect(player.x, player.y, player.width, player.height);
 
-    context.fillStyle = "white";
+    context.fillStyle = "yellow";//ball drawing
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
     context.fillRect(ball.x, ball.y, ball.width, ball.height);
@@ -99,14 +99,15 @@ function update() {
         ball.velocityX *= -1; 
     }
     else if (ball.y + ball.height >= boardHeight) {
-        
-        context.font = "20px sans-serif";
-        context.fillText("Game Over: Press 'Space' to Restart", 80, 400);
+        context.fillStyle ="greenyellow";
+        context.font = "30px sans-serif";
+        context.fillText("GAME OVER :(  PRESSING 'SPACE' RESTARTS THE GAME", 10, 400);
+  
         gameOver = true;
     }
 
   
-    context.fillStyle = "skyblue";
+    context.fillStyle = "aqua";
     for (let i = 0; i < blockArray.length; i++) {
         let block = blockArray[i];
         if (!block.break) {
@@ -125,6 +126,9 @@ function update() {
             context.fillRect(block.x, block.y, block.width, block.height);
         }
     }
+    context.fillStyle = "yellow";
+    context.font = "25px sans-serif";
+    context.fillText("Score : ",10,20)
 
 
     if (blockCount == 0) {
@@ -134,8 +138,8 @@ function update() {
     }
 
     
-    context.font = "20px sans-serif";
-    context.fillText(score, 10, 25);
+    context.font = "25px sans-serif";
+    context.fillText(score, 90, 20);
 }
 
 function outOfBounds(xPosition) {
